a8e24b579abbfc489b3d0f5d7471f2db0f1d9310
Raludza <rale.ilic1414@gmail.com>
2016-05-12 04:51:22 +0200

    Dodata forma za korisnike.

---
+++ src/actions/CommitAction.java
@@ -0,0 +1,31 @@
+package actions;
+
+import form.Form;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.sql.SQLException;
+
+import javax.swing.AbstractAction;
+import javax.swing.JDialog;
+import javax.swing.KeyStroke;
+
+public class CommitAction extends AbstractAction{
+
+private JDialog standardForm;
+	
+	public CommitAction(JDialog standardForm) {
+		KeyStroke ctrlDKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK);
+		putValue(ACCELERATOR_KEY,ctrlDKeyStroke);
+		putValue(SHORT_DESCRIPTION, "Commit");
+		putValue(NAME, "Commit");
+		
+		this.standardForm=standardForm;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {	
+		
+		standardForm.dispose();
+		
+	}
+}
